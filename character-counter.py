0ee4f5bc958dfe0419a83a310bb0ed98c23d1407
MASKY-33 <masky.somebody33@gmail.com>
2026-06-20 16:10:38 +0200

    Create character-counter.py

---
+++ character-counter.py
@@ -0,0 +1,10 @@
+## This Program:
+# - asks a sentence
+# - counts how many characters there are in Total (space included)
+# - prints all characters (space included)
+
+ask_sentence = input("Type any sentence: ")
+
+count = 0
+
+print(len(ask_sentence))
